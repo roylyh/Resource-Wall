ab0684733af05aca3ae02ef977da3257ef62ed8b
Spencer Hahmo <schahmo@gmail.com>
2023-03-13 19:46:58 -0600

    Running db:reset now works properly

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -1,5 +1,5 @@
 -- Resources table seeds here
-INSERT INTO resources (user_id, topic_id, url, title, description) VALUES (1, 4, 'image placeholder link', 'https://www.youtube.com/watch?v=TF6cnLnEARo', 'funtwo - Canon', 'One of the coolest YouTube vidoes'),
+INSERT INTO resources (user_id, topic_id, img_url, url, title, description) VALUES (1, 4, 'image placeholder link', 'https://www.youtube.com/watch?v=TF6cnLnEARo', 'funtwo - Canon', 'One of the coolest YouTube vidoes'),
 (2, 1, 'image placeholder link', 'url placeholder link', 'title', 'description'),
 (2, 2, 'image placeholder link', 'url placeholder link', 'title', 'description'),
 (3, 3, 'image placeholder link', 'url placeholder link', 'title', 'description');
