324ca0d5c43d04712fa44a3f942f17486ef19d1c
roylyh <roy.louyihui@gmail.com>
2023-03-14 14:02:13 -0700

    debug resetdb.js

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -12,29 +12,29 @@ const db = require('../db/connection');
 // const client = new Client();
 
 // Loads the schema files from db/schema
-const runSchemaFiles = async () => {
+const runSchemaFiles = async() => {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
   const schemaFilenames = fs.readdirSync('./db/schema');
 
   for (const fn of schemaFilenames) {
     const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
-    await db.query(sql);
+    await db.query(sql,null,()=>{});
   }
 };
 
-const runSeedFiles = async () => {
+const runSeedFiles = async() => {
   console.log(chalk.cyan(`-> Loading Seeds ...`));
   const schemaFilenames = fs.readdirSync('./db/seeds');
 
   for (const fn of schemaFilenames) {
     const sql = fs.readFileSync(`./db/seeds/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
-    await db.query(sql);
+    await db.query(sql,null,()=>{});
   }
 };
 
-const runResetDB = async () => {
+const runResetDB = async() => {
   try {
     process.env.DB_HOST &&
       console.log(`-> Connecting to PG on ${process.env.DB_HOST} as ${process.env.DB_USER}...`);
