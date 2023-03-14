209f1c177130dbbe1f2b8551d927d3b115956857
roylyh <roy.louyihui@gmail.com>
2023-03-14 15:51:04 -0700

    update

--- db/queries/resource-queries.js
+++ db/queries/resource-queries.js
@@ -12,3 +12,73 @@ const getAllResouces = () => {
 };
 
 exports.getAllResouces = getAllResouces;
+
+const getMyfavorites = (userId) => {
+  const queryString = `select x.* from resources x join likes y on x.id = y.resource_id where y.user_id = $1;`;
+  const queryParam = [userId];
+  return db.query(queryString,queryParam,(res) => {
+    return res.rows;
+  }
+  ).catch((err) => {
+    console.log(err.message);
+  }
+  );
+};
+
+exports.getMyfavorites = getMyfavorites;
+
+const getMyresources = (userId) => {
+  const queryString = `select * from resources where user_id = $1;`;
+  const queryParam = [userId];
+  return db.query(queryString,queryParam,(res) => {
+    return res.rows;
+  }
+  ).catch((err) => {
+    console.log(err.message);
+  }
+  );
+};
+
+exports.getMyresources = getMyresources;
+
+const getComments = (resourceId) => {
+  const queryString = `select * from comments where resource_id = $1;`;
+  const queryParam = [resourceId];
+  return db.query(queryString,queryParam,(res) => {
+    return res.rows;
+  }
+  ).catch((err) => {
+    console.log(err.message);
+  }
+  );
+};
+
+exports.getComments = getComments;
+
+const likeResource = (resourceId, userId) => {
+  const queryString = `INSERT INTO likes (user_id, resource_id, liked) VALUES ($1, $2, TRUE) RETURNING *;`;
+  const queryParam = [userId, resourceId];
+  return db.query(queryString,queryParam,(res) => {
+    return res.rows;
+  }
+  ).catch((err) => {
+    console.log(err.message);
+  }
+  );
+};
+
+exports.likeResource = likeResource;
+
+const rateResource = (resourceId, rate, userId) => {
+  const queryString = `INSERT INTO ratings (user_id, resource_id, rating) VALUES ($1, $2, $3) RETURNING *;`;
+  const queryParam = [userId, resourceId, rate];
+  return db.query(queryString,queryParam,(res) => {
+    return res.rows;
+  }
+  ).catch((err) => {
+    console.log(err.message);
+  }
+  );
+};
+
+exports.rateResource = rateResource;
--- routes/resources.js
+++ routes/resources.js
@@ -14,4 +14,84 @@ router.get('/allresources', (req, res) => {
     });
 });
 
+router.get('/myfavorites', (req, res) => {
+  // const userId = req.session.userId;
+  const userId = 2;
+  resourceQueries.getMyfavorites(userId)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+router.get('/myresources', (req, res) => {
+  // const userId = req.session.userId;
+  const userId = 2;
+  resourceQueries.getMyresources(userId)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+router.get('/allcomments/:resource_id', (req, res) => {
+  resourceQueries.getComments(req.params.resource_id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+router.get('/allcomments/:resource_id', (req, res) => {
+  resourceQueries.getComments(req.params.resource_id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+router.get('/likeresource/:resource_id', (req, res) => {
+  // const userId = req.session.userId;
+  const userId = 2;
+  resourceQueries.likeResource(req.params.resource_id, userId)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+router.get('/rateresource/:resource_id/:rate', (req, res) => {
+  // const userId = req.session.userId;
+  const userId = 2;
+  resourceQueries.rateResource(req.params.resource_id, req.params.rate, userId)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
 module.exports = router;
