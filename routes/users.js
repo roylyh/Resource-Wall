017f2afd675b09cfd1302c76e16d2e3e6f063f78
roylyh <roy.louyihui@gmail.com>
2023-03-15 00:20:02 -0700

    change users-queries to user-queries

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const userQueries = require('../db/queries/users-queries');
+const userQueries = require('../db/queries/user-queries');
 const bcrypt = require("bcryptjs");
 
 router.get('/login/:id', (req, res) => {
