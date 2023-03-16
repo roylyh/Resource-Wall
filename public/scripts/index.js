ea61fba9039a157c59b73683d05cee52af401738
roylyh <roy.louyihui@gmail.com>
2023-03-16 14:51:16 -0700

    debug index page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,11 @@
 // Client facing scripts here
 $(() => {
-
   for (let topic_id = 1; topic_id < 5; topic_id++) {
-    getAllResourcesByTopic(topic_id, 0)
-      .then((json) => {
-        let $resItem = "";
-        json.forEach(resource => {
-          console.log(resource);
-          $resItem += `
+    getAllResourcesByTopic(topic_id, 0).then((json) => {
+      let $resItem = "";
+      json.forEach((resource) => {
+        console.log(resource);
+        $resItem += `
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
           <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><image href=${resource.img_url} height="100%" width="100%" /></svg>
@@ -24,12 +22,9 @@ $(() => {
           </div>
         </div>
       `;
-        });
-        $("#row" + topic_id).html($resItem);
-      }
-      );
+      });
+      $("#row" + topic_id).html($resItem);
+    });
   }
-
-}
-);
-
+  $("#homeli").addClass("active");
+});
